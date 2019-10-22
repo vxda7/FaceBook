3672642a0eb951dae695116849128a00cb151883
vxda77 <vxda77@gmail.com>
2019-10-22 10:16:24 +0900

    model 선언

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
