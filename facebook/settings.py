d9decbc2f40d4534354850a75423256360e3c9bd
vxda77 <vxda77@gmail.com>
2019-10-22 09:23:26 +0900

    accounts 앱 추가!

--- facebook/settings.py
+++ facebook/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'posts',
+    'accounts',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
--- facebook/urls.py
+++ facebook/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, include
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from posts import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls')),
-    path('', views.index)
+    path('', views.index),
+    path('accounts/', include('accounts.urls')),
 ]
