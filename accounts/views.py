212b6a559e2c629eb823eb2a96ba16c4ce43fb44
vxda77 <vxda77@gmail.com>
2019-10-22 10:03:02 +0900

    login + logout 기능 구현 완료

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 # Create your views here.
@@ -16,3 +17,22 @@ def signup(request):
         'form': form,
     }
     return render(request, 'accounts/form.html', context)
+
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('posts:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/form.html', context)
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('posts:index')
